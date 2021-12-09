fb1f70b206527b2250e8c663a6fa820a053027f9
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-09 19:26:12 +0200

    added ConfigProvider

---
+++ Module3HW2/Module3HW2/Providers/ConfigProvider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using Module3HW2.Models;
+using Module3HW2.Providers.Abstractions;
+using Module3HW2.Services.Abstarctions;
+
+namespace Module3HW2.Providers
+{
+    public class ConfigProvider : IConfigProvider
+    {
+        private const string ConfigFile = "config.json";
+
+        private readonly Config _config;
+        private readonly IFileConversionService _fileConversionService;
+
+        public ConfigProvider(IFileConversionService fileConversionService)
+        {
+            _fileConversionService = fileConversionService;
+
+            _config = LoadConfig();
+        }
+
+        public Config Config => _config;
+
+        private Config LoadConfig()
+        {
+            return _fileConversionService.ConvertJsonToConfig(ConfigFile);
+        }
+    }
+}
