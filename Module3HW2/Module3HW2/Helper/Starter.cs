f23752d3ad442bfc07d01841fdad7264afd15cee
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-09 19:29:03 +0200

    added Starter

---
+++ Module3HW2/Module3HW2/Helper/Starter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Module3HW2.Providers;
+using Module3HW2.Providers.Abstractions;
+using Module3HW2.Services;
+using Module3HW2.Services.Abstarctions;
+
+namespace Module3HW2.Helper
+{
+    public class Starter
+    {
+        public void Run()
+        {
+            var start = new ServiceCollection()
+                .AddTransient<IConfigProvider, ConfigProvider>()
+                .AddSingleton<IConfigService, ConfigService>()
+                .AddTransient<IFileConversionService, JsonConversionService>()
+                .AddTransient<App>()
+                .BuildServiceProvider();
+            var application = start.GetService<App>();
+            application.SimulateWork();
+        }
+    }
+}
