89aab7cd9bc3dbe081e6e8b981cdb3a982b6c973
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-09 19:28:47 +0200

    added Models

---
+++ Module3HW2/Module3HW2/Models/Config.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Module3HW2.Models
+{
+    public class Config
+    {
+        public CultureConfig CultureConfig { get; set; }
+    }
+}
---
+++ Module3HW2/Module3HW2/Models/Contact.cs
@@ -0,0 +1,11 @@
+﻿namespace Module3HW2.Models
+{
+    public class Contact
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
