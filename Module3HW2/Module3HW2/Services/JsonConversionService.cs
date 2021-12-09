973ac405a1d0c5419d7516c765b90de2ae7a2bd5
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-09 19:28:36 +0200

    added JsonConversionService

---
+++ Module3HW2/Module3HW2/Services/JsonConversionService.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using Module3HW2.Models;
+using Module3HW2.Services.Abstarctions;
+using Newtonsoft.Json;
+
+namespace Module3HW2.Services
+{
+    public class JsonConversionService : IFileConversionService
+    {
+        public Config ConvertJsonToConfig(string jsonConfig)
+        {
+            var configFile = File.ReadAllText(jsonConfig);
+            var config = JsonConvert.DeserializeObject<Config>(configFile);
+            return config;
+        }
+    }
+}
