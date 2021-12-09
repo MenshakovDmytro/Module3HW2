e45446b287d6754fe86adade9e1bbfb696d76276
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-09 19:28:12 +0200

    added ConfigService

---
+++ Module3HW2/Module3HW2/Services/ConfigService.cs
@@ -0,0 +1,18 @@
+﻿using Module3HW2.Models;
+using Module3HW2.Providers.Abstractions;
+using Module3HW2.Services.Abstarctions;
+
+namespace Module3HW2.Services
+{
+    public class ConfigService : IConfigService
+    {
+        private readonly IConfigProvider _configProvider;
+
+        public ConfigService(IConfigProvider configProvider)
+        {
+            _configProvider = configProvider;
+        }
+
+        public CultureConfig CultureConfig => _configProvider.Config.CultureConfig;
+    }
+}
