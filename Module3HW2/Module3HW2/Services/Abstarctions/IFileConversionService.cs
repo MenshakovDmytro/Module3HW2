13dd95405113876c2b56a222c36cff81ec001e82
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-09 19:26:50 +0200

    added IFileConversionService

---
+++ Module3HW2/Module3HW2/Services/Abstarctions/IFileConversionService.cs
@@ -0,0 +1,9 @@
+﻿using Module3HW2.Models;
+
+namespace Module3HW2.Services.Abstarctions
+{
+    public interface IFileConversionService
+    {
+        public Config ConvertJsonToConfig(string jsonConfig);
+    }
+}
