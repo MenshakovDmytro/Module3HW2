7698617705fca6a666a7c2b202f4ef6f7cd31dd4
MenshakovDmytro <jgadf1kz@gmail.com>
2021-12-09 19:29:16 +0200

    updated main project

--- Module3HW2/Module3HW2/Program.cs
+++ Module3HW2/Module3HW2/Program.cs
@@ -1,12 +1,13 @@
-﻿using System;
+﻿using Module3HW2.Helper;
 
 namespace PhoneBook
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var start = new Starter();
+            start.Run();
         }
     }
 }
